5c0247ae687189f65794711142efbc9f164f0838
Jelani Jackson <jelani.jackson@lifeomic.com>
2020-10-11 20:04:32 -0500

    Testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+import dotenv from 'dotenv';
+import Client from './client';
 
+dotenv.config();
+
+const client = new Client(process.env.TD_CONSUMER_KEY as string, 'http://127.0.0.1');
+
+console.log(client);
+
+client.authenticate();
