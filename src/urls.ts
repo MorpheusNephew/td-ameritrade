4cf38a0a1bbe2f173ac6aadac575448a5c413ec1
Jelani Jackson <jelani.jackson@lifeomic.com>
2020-10-11 20:03:51 -0500

    Added getAuthUrl

--- src/urls.ts
+++ src/urls.ts
@@ -1,3 +1,6 @@
-const baseUrl = "https://api.tdameritrade.com/v1";
+const hostname = 'tdameritrade.com';
+const baseApiUrl = `https://api.${hostname}/v1`;
+const baseAuthUrl = `https://auth.${hostname}`;
+export const accessTokenUrl = `${baseApiUrl}/oauth2/token`;
 
-export const accessTokenUrl = `${baseUrl}/oauth2/token`;
+export const getAuthUrl = (clientId: string, redirectUri:string) => `${baseAuthUrl}/auth?response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}%40AMER.OAUTHAP`;
