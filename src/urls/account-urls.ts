17a2412d28a0ebaf0fffb929ea612eb4eb85dce1
Jelani Jackson <jeljack86@gmail.com>
2020-10-24 06:52:53 -0500

    Updated accounts url to take into account fields querystring params

--- src/urls/account-urls.ts
+++ src/urls/account-urls.ts
@@ -1,7 +1,25 @@
+import { stringify } from 'qs';
 import { baseApiUrl, numberOrString } from '.';
 
-export const getAllAccountsUrl = (): string => `${baseApiUrl}/accounts`;
+export const getAllAccountsUrl = (fields?: string[]): string => {
+  const params = stringify({
+    fields,
+  });
 
-export const getAccountUrl = (accountId: numberOrString): string => {
-  return `${getAllAccountsUrl()}/${accountId}`;
+  const url = `${baseApiUrl}/accounts`;
+
+  return params ? `${url}?${params}` : url;
+};
+
+export const getAccountUrl = (
+  accountId: numberOrString,
+  fields?: string[]
+): string => {
+  const params = stringify({
+    fields,
+  });
+
+  const url = `${getAllAccountsUrl()}/${accountId}`;
+
+  return params ? `${url}?${params}` : url;
 };
