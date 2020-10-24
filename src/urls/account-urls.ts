794b7fe59a547376cf66c48f3f939b7f43771a82
Jelani Jackson <jeljack86@gmail.com>
2020-10-24 07:08:20 -0500

    Letting the urls be urls

--- src/urls/account-urls.ts
+++ src/urls/account-urls.ts
@@ -1,25 +1,7 @@
-import { stringify } from 'qs';
 import { baseApiUrl, numberOrString } from '.';
 
-export const getAllAccountsUrl = (fields?: string[]): string => {
-  const params = stringify({
-    fields,
-  });
+export const getAllAccountsUrl = (): string => `${baseApiUrl}/accounts`;
 
-  const url = `${baseApiUrl}/accounts`;
-
-  return params ? `${url}?${params}` : url;
-};
-
-export const getAccountUrl = (
-  accountId: numberOrString,
-  fields?: string[]
-): string => {
-  const params = stringify({
-    fields,
-  });
-
-  const url = `${getAllAccountsUrl()}/${accountId}`;
-
-  return params ? `${url}?${params}` : url;
+export const getAccountUrl = (accountId: numberOrString): string => {
+  return `${getAllAccountsUrl()}/${accountId}`;
 };
