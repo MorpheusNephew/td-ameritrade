f67bd757057085ec0e120f935e8ceae6641e858d
Jelani Jackson <jeljack86@gmail.com>
2020-10-28 23:03:56 -0500

    Added clients to main client

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -2,6 +2,16 @@ import { AxiosResponse } from 'axios';
 import AuthClient from './auth-client';
 import AccountsClient from './accounts-client';
 import InstrumentsClient from './instruments-client';
+import MarketHoursClient from './market-hours-client';
+import MoversClient from './movers-client';
+import OptionChainsClient from './option-chains-client';
+import OrdersClient from './orders-client';
+import PriceHistoryClient from './price-history-client';
+import QuotesClient from './quotes-client';
+import SavedOrdersClient from './saved-orders-client';
+import TransactionHistoryClient from './transaction-history-client';
+import UserInfoClient from './user-info-client';
+import WatchlistClient from './watchlist-client';
 
 export interface ClientOptions {
   clientId: string;
@@ -18,6 +28,16 @@ export default class TdAmeritradeClient {
   accounts: AccountsClient;
   auth: AuthClient;
   instruments: InstrumentsClient;
+  marketHours: MarketHoursClient;
+  movers: MoversClient;
+  optionChains: OptionChainsClient;
+  orders: OrdersClient;
+  priceHistory: PriceHistoryClient;
+  quotes: QuotesClient;
+  savedOrders: SavedOrdersClient;
+  transactionHistory: TransactionHistoryClient;
+  userInfo: UserInfoClient;
+  watchlist: WatchlistClient;
 
   constructor(options: ClientOptions) {
     const { clientId, redirectUri, accessToken, refreshToken } = options;
@@ -29,6 +49,16 @@ export default class TdAmeritradeClient {
     this.accounts = new AccountsClient(this);
     this.auth = new AuthClient(this);
     this.instruments = new InstrumentsClient(this);
+    this.marketHours = new MarketHoursClient(this);
+    this.movers = new MoversClient(this);
+    this.optionChains = new OptionChainsClient(this);
+    this.orders = new OrdersClient(this);
+    this.priceHistory = new PriceHistoryClient(this);
+    this.quotes = new QuotesClient(this);
+    this.savedOrders = new SavedOrdersClient(this);
+    this.transactionHistory = new TransactionHistoryClient(this);
+    this.userInfo = new UserInfoClient(this);
+    this.watchlist = new WatchlistClient(this);
   }
 
   _makeRequest = async <T>(
