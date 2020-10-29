012ca3a47aeb3b74e6c55e26c507afd95fe61d53
Jelani Jackson <jeljack86@gmail.com>
2020-10-29 07:09:54 -0500

    Added fields query parameters

--- src/clients/accounts-client.ts
+++ src/clients/accounts-client.ts
@@ -1,7 +1,10 @@
 import { Account } from '@morpheusnephew/td-ameritrade-models';
 import Axios, { AxiosResponse } from 'axios';
+import qs from 'qs';
 import TdAmeritradeClient from '.';
 import { getAccountUrl, getAllAccountsUrl, numberOrString } from '../urls';
+
+export type Fields = 'positions' | 'orders';
 
 export default class AccountsClient {
   private _client: TdAmeritradeClient;
@@ -10,16 +13,37 @@ export default class AccountsClient {
     this._client = client;
   }
 
-  getAllAccounts = (): Promise<AxiosResponse<Account[]>> => {
-    const url = getAllAccountsUrl();
+  getAllAccounts = (fields?: Fields[]): Promise<AxiosResponse<Account[]>> => {
+    let queryString = null;
+
+    if (fields) {
+      const fieldsParam = fields.join(',');
+
+      queryString = qs.stringify({ fields: fieldsParam });
+    }
+
+    const url = `${getAllAccountsUrl()}${queryString ? `?${queryString}` : ''}`;
 
     return this._client._makeRequest(
       async (authConfig) => await Axios.get<Account[]>(url, authConfig)
     );
   };
 
-  getAccount = (accountId: numberOrString): Promise<AxiosResponse<Account>> => {
-    const url = getAccountUrl(accountId);
+  getAccount = (
+    accountId: numberOrString,
+    fields?: Fields[]
+  ): Promise<AxiosResponse<Account>> => {
+    let queryString = null;
+
+    if (fields) {
+      const fieldsParam = fields.join(',');
+
+      queryString = qs.stringify({ fields: fieldsParam });
+    }
+
+    const url = `${getAccountUrl(accountId)}${
+      queryString ? `?${queryString}` : ''
+    }`;
 
     return this._client._makeRequest(
       async (authConfig) => await Axios.get<Account>(url, authConfig)
