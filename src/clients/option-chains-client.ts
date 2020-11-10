948ab7f99356f136408f8fecf7fa0fe22b5086cd
Jelani Jackson <jeljack86@gmail.com>
2020-11-09 20:22:43 -0500

    Filled option chain client

--- src/clients/option-chains-client.ts
+++ src/clients/option-chains-client.ts
@@ -1,4 +1,55 @@
+import { OptionChain } from '@morpheusnephew/td-ameritrade-models';
+import Axios, { AxiosResponse } from 'axios';
+import qs from 'qs';
 import TdAmeritradeClient from '.';
+import { queryStringOptions } from '../config';
+import { numberOrString } from '../urls';
+import { getOptionChainUrl } from '../urls/options-url';
+
+export interface OptionChainOptions {
+  apiKey?: string;
+  symbol: string;
+  contractType?: 'ALL' | 'CALL' | 'PUT';
+  strikeCount: numberOrString;
+  includeQuotes?: boolean;
+  strategy?:
+    | 'SINGLE'
+    | 'ANALYTICAL'
+    | 'COVERED'
+    | 'VERTICAL'
+    | 'CALENDAR'
+    | 'STRANGLE'
+    | 'STRADDLE'
+    | 'BUTTERFLY'
+    | 'CONDOR'
+    | 'DIAGONAL'
+    | 'COLLAR'
+    | 'ROLL';
+  interval: numberOrString;
+  strike: numberOrString;
+  range?: 'ALL' | 'ITM' | 'NTM' | 'OTM' | 'SAK' | 'SBK' | 'SNK';
+  fromDate: Date;
+  toDate: Date;
+  volatility: numberOrString;
+  underlyingPrice: numberOrString;
+  interestRate: numberOrString;
+  daysToExpiration: numberOrString;
+  expMonth?:
+    | 'ALL'
+    | 'JAN'
+    | 'FEB'
+    | 'MAR'
+    | 'APR'
+    | 'MAY'
+    | 'JUN'
+    | 'JUL'
+    | 'AUG'
+    | 'SEP'
+    | 'OCT'
+    | 'NOV'
+    | 'DEC';
+  optionType?: 'ALL' | 'S' | 'NS';
+}
 
 export default class OptionChainsClient {
   private _client: TdAmeritradeClient;
@@ -7,5 +58,15 @@ export default class OptionChainsClient {
     this._client = client;
   }
 
-  getOptionChain = () => {};
+  getOptionChain = (
+    optionChainOptions: OptionChainOptions
+  ): Promise<AxiosResponse<any>> => {
+    const queryString = qs.stringify(optionChainOptions, queryStringOptions);
+
+    const url = `${getOptionChainUrl()}${queryString}`;
+
+    return this._client._makeRequest(
+      async (authConfig) => await Axios.get(url, authConfig)
+    );
+  };
 }
