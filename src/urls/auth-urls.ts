f4a6b99bebacd683c3ec5f981dc82eac7be4df96
Jelani Jackson <jeljack86@gmail.com>
2020-11-27 10:04:04 -0500

    Added state to auth-urls

--- src/urls/auth-urls.ts
+++ src/urls/auth-urls.ts
@@ -1,16 +1,29 @@
+import qs from 'qs';
 import { getBaseApiUrl, getHostname } from '.';
+import { queryStringOptions } from '../config';
 
 export const getAccessTokenUrl = () => `${getBaseApiUrl()}/oauth2/token`;
 
 export interface authOptions {
   client_id: string;
   redirect_uri: string;
+  state?: string;
 }
 
 const getBaseAuthUrl = () => `https://auth.${getHostname()}`;
 
 export const getAuthUrl = (options: authOptions): string => {
-  const { redirect_uri: redirectUri, client_id: clientId } = options;
+  const { redirect_uri, client_id, state } = options;
 
-  return `${getBaseAuthUrl()}/auth?response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}%40AMER.OAUTHAP`;
+  const params = qs.stringify(
+    {
+      response_type: 'code',
+      client_id: `${client_id}%40AMER.OAUTHAP`,
+      redirect_uri,
+      state,
+    },
+    queryStringOptions
+  );
+
+  return `${getBaseAuthUrl()}/auth${params}`;
 };
