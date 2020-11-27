ddfcb81bd0a37b360720c1fad0a31ac32d42a90f
Jelani Jackson <jeljack86@gmail.com>
2020-11-27 08:02:16 -0500

    Saved orders client not implemented

--- tests/clients/saved-orders-client.test.ts
+++ tests/clients/saved-orders-client.test.ts
@@ -4,7 +4,7 @@ import TdAmeritradeClient, { ClientOptions } from '../../src/clients';
 
 jest.mock('axios');
 
-describe('{name of client} client tests', () => {
+describe('Saved orders client tests', () => {
   let mockedAxios: jest.Mocked<typeof axios>;
   let client: TdAmeritradeClient;
 
@@ -14,5 +14,31 @@ describe('{name of client} client tests', () => {
     mockedAxios = axios as jest.Mocked<typeof axios>;
   });
 
-  // insert tests
+  it('should throw notImplemented when createSavedOrder is called', async () => {
+    expect(() => client.savedOrders.createSavedOrder()).toThrow(
+      'Not Implemented'
+    );
+  });
+
+  it('should throw notImplemented when deleteSavedOrder is called', async () => {
+    expect(() => client.savedOrders.deleteSavedOrder()).toThrow(
+      'Not Implemented'
+    );
+  });
+
+  it('should throw notImplemented when getSavedOrder is called', async () => {
+    expect(() => client.savedOrders.getSavedOrder()).toThrow('Not Implemented');
+  });
+
+  it('should throw notImplemented when getSavedOrdersByPath is called', async () => {
+    expect(() => client.savedOrders.getSavedOrdersByPath()).toThrow(
+      'Not Implemented'
+    );
+  });
+
+  it('should throw notImplemented when replaceSavedOrder is called', async () => {
+    expect(() => client.savedOrders.replaceSavedOrder()).toThrow(
+      'Not Implemented'
+    );
+  });
 });
