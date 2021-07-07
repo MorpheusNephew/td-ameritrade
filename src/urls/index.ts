fe0ebe6aa4486661229e02738fde305052e297a5
Jelani Jackson <jeljack86@gmail.com>
2021-07-07 05:43:56 -0400

    Updated filenames

--- src/urls/index.ts
+++ src/urls/index.ts
@@ -2,7 +2,7 @@ export * from './auth-urls';
 export * from './account-urls';
 export * from './instrument-urls';
 export * from './market-hours-urls';
-export * from './movers';
+export * from './movers-urls';
 export * from './order-urls';
 
 export const getHostname = () => 'tdameritrade.com';
