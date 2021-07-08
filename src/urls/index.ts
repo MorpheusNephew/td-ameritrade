2e932602ac31e05fed271c22d0df43ca68444dcd
Jelani Jackson <jeljack86@gmail.com>
2021-07-07 20:05:41 -0400

    Added price history to url index

--- src/urls/index.ts
+++ src/urls/index.ts
@@ -4,6 +4,7 @@ export * from './instrument-urls';
 export * from './market-hours-urls';
 export * from './movers-urls';
 export * from './order-urls';
+export * from './price-history-urls';
 
 export const getHostname = () => 'tdameritrade.com';
 export const getBaseApiUrl = () => `https://api.${getHostname()}/v1`;
