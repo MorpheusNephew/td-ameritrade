d87bb9902d5baf4d8e31b6fcfd37ec864da2df71
Jelani Jackson <jeljack86@gmail.com>
2021-08-02 09:37:09 -0400

    :sparkles: added functionality to refresh access and refresh tokens

--- src/clients/auth-client.ts
+++ src/clients/auth-client.ts
@@ -1,11 +1,11 @@
+import TdAmeritradeClient from '.';
+import { getAccessTokenUrl } from '..';
 import Axios, { AxiosResponse } from 'axios';
 import qs from 'qs';
 import {
   Token as AccessTokenResponse,
   TokenRequest as AccessTokenRequest,
 } from '@morpheusnephew/td-ameritrade-models';
-import TdAmeritradeClient from '.';
-import { getAccessTokenUrl } from '..';
 
 export default class AuthClient {
   private _client: TdAmeritradeClient;
@@ -14,7 +14,7 @@ export default class AuthClient {
     this._client = client;
   }
 
-  authenticate = async (
+  authenticate = (
     code: string,
     state?: string
   ): Promise<AxiosResponse<AccessTokenResponse>> => {
@@ -27,6 +27,31 @@ export default class AuthClient {
       state: state,
     };
 
+    return this._request(body);
+  };
+
+  refreshAccessToken = (): Promise<AxiosResponse<AccessTokenResponse>> => {
+    const body: AccessTokenRequest = {
+      client_id: this._client.clientId,
+      grant_type: 'refresh_token',
+      refresh_token: this._client.refreshToken,
+    };
+
+    return this._request(body);
+  };
+
+  refreshRefreshToken = (): Promise<AxiosResponse<AccessTokenResponse>> => {
+    const body: AccessTokenRequest = {
+      access_type: 'offline',
+      client_id: this._client.clientId,
+      grant_type: 'refresh_token',
+      refresh_token: this._client.refreshToken,
+    };
+
+    return this._request(body);
+  };
+
+  _request = async (body: AccessTokenRequest) => {
     const response = await Axios.post<AccessTokenResponse>(
       getAccessTokenUrl(),
       qs.stringify(body),
--- tests/clients/auth-client.test.ts
+++ tests/clients/auth-client.test.ts
@@ -1,7 +1,10 @@
-import { createMock } from 'ts-auto-mock';
-import Axios from 'axios';
+import { getAccessTokenUrl } from '../../src';
 import TdAmeritradeClient, { ClientOptions } from '../../src/clients';
-import { Token } from '@morpheusnephew/td-ameritrade-models';
+import { Token, TokenRequest } from '@morpheusnephew/td-ameritrade-models';
+import Axios, { AxiosResponse } from 'axios';
+import qs from 'qs';
+import { createMock } from 'ts-auto-mock';
+import { v4 as uuid } from 'uuid';
 
 jest.mock('axios');
 
@@ -15,15 +18,134 @@ describe('Auth client tests', () => {
     mockedAxios = Axios as jest.Mocked<typeof Axios>;
   });
 
-  it('should authenticate', async () => {
-    const expectedResult = createMock<Token>();
+  describe('_request()', () => {
+    it('should make auth request', async () => {
+      const tokenRequest = createMock<TokenRequest>();
+      const expectedResult = createMock<Token>();
 
-    mockedAxios.post.mockResolvedValueOnce({
-      data: expectedResult,
+      mockedAxios.post.mockResolvedValueOnce({
+        data: expectedResult,
+      });
+
+      const { data } = await client.auth._request(tokenRequest);
+
+      expect(data).toBe(expectedResult);
     });
 
-    const { data } = await client.auth.authenticate('code');
+    it('should make request with proper parameters', async () => {
+      const tokenRequest = createMock<TokenRequest>();
 
-    expect(data).toBe(expectedResult);
+      mockedAxios.post.mockResolvedValueOnce({
+        data: null,
+      });
+
+      await client.auth._request(tokenRequest);
+
+      expect(mockedAxios.post).toBeCalledWith(
+        getAccessTokenUrl(),
+        qs.stringify(tokenRequest),
+        {
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        }
+      );
+    });
+
+    it('should only update access_token', async () => {
+      const tokenRequest = createMock<TokenRequest>();
+      const expectedResult = createMock<Token>();
+
+      expectedResult.access_token = uuid();
+
+      mockedAxios.post.mockResolvedValueOnce({
+        data: expectedResult,
+      });
+
+      const { accessToken, refreshToken } = client;
+
+      await client.auth._request(tokenRequest);
+
+      expect(client.accessToken).not.toBe(accessToken);
+      expect(client.refreshToken).toBe(refreshToken);
+    });
+
+    it('should only update refresh token', async () => {
+      const tokenRequest = createMock<TokenRequest>();
+      const expectedResult = createMock<Token>();
+
+      expectedResult.refresh_token = uuid();
+
+      mockedAxios.post.mockResolvedValueOnce({
+        data: expectedResult,
+      });
+
+      const { accessToken, refreshToken } = client;
+
+      await client.auth._request(tokenRequest);
+
+      expect(client.accessToken).toBe(accessToken);
+      expect(client.refreshToken).not.toBe(refreshToken);
+    });
+  });
+
+  describe('authenticate()', () => {
+    it('should make request with proper parameters', async () => {
+      const code = uuid();
+      const state = uuid();
+      const response = createMock<AxiosResponse<Token>>();
+
+      const expectedParams: TokenRequest = {
+        client_id: client.clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: client.redirectUri,
+        access_type: 'offline',
+        state,
+      };
+
+      client.auth._request = jest.fn().mockResolvedValueOnce(response);
+
+      await client.auth.authenticate(code, state);
+
+      expect(client.auth._request).toBeCalledWith(expectedParams);
+    });
+  });
+
+  describe('refreshAccessToken()', () => {
+    it('should make request with proper parameters', async () => {
+      const response = createMock<AxiosResponse<Token>>();
+
+      const expectedParams: TokenRequest = {
+        client_id: client.clientId,
+        grant_type: 'refresh_token',
+        refresh_token: client.refreshToken,
+      };
+
+      client.auth._request = jest.fn().mockResolvedValueOnce(response);
+
+      await client.auth.refreshAccessToken();
+
+      expect(client.auth._request).toBeCalledWith(expectedParams);
+    });
+  });
+
+  describe('refreshRefreshToken()', () => {
+    it('should make request with proper parameters', async () => {
+      const response = createMock<AxiosResponse<Token>>();
+
+      const expectedParams: TokenRequest = {
+        access_type: 'offline',
+        client_id: client.clientId,
+        grant_type: 'refresh_token',
+        refresh_token: client.refreshToken,
+      };
+
+      client.auth._request = jest.fn().mockResolvedValueOnce(response);
+
+      await client.auth.refreshRefreshToken();
+
+      expect(client.auth._request).toBeCalledWith(expectedParams);
+    });
   });
 });
