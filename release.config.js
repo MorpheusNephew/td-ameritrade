e0fb62672de181c31e7ddf43a8a78b067a2ffc40
Jelani Jackson <jelani.jackson@lifeomic.com>
2022-03-19 17:22:32 -0500

    Updated release config

--- release.config.js
+++ release.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    ['@semantic-release/commit-analyzer', { preset: 'conventionalcommits' }],
-    [
-      '@semantic-release/release-notes-generator',
-      { preset: 'conventionalcommits' },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     [
       '@semantic-release/changelog',
       {
